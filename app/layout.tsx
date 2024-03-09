10ca206fb7a5bae2badb7068c29463633410f3e7
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-03-09 14:21:41 +0000

    chore: add umami tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { GeistMono } from "geist/font/mono";
+import Script from "next/script";
 
 export const metadata: Metadata = {
     title: "Eight Labs",
@@ -16,6 +17,11 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={GeistMono.className}>{children}</body>
+            <Script
+                async
+                src="https://analytics.duncan.land/script.js"
+                data-website-id="52165d92-3bfb-4efd-b6ee-1204801937e0"
+            />
         </html>
     );
 }
